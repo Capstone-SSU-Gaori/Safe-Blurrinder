d94d066f2e6c2c22918b5a2178c3a1452d8f81d1
Song-EunJu <jd06280@naver.com>
2021-11-29 23:01:19 +0900

    [feat] spring->flask 영상전송 진행중

--- src/main/java/com/example/SafeBlurrinder/controller/VideoController.java
+++ src/main/java/com/example/SafeBlurrinder/controller/VideoController.java
@@ -33,14 +33,45 @@ public class VideoController {
         return "uploadVideoForm";
     }
 
+//    //파일 업로드 후, submit 클릭 시 여기로 이동
+//    @PostMapping("/uploadVideo")
+//    public String saveVideo(@RequestParam("file") MultipartFile files) throws IOException {
+//        String origName=files.getOriginalFilename();
+////        System.out.println("original name: "+origName);
+//        String[] splited=origName.split("\\.");
+//        String saveName=new GenerateHash(origName).out()+"."+splited[splited.length-1];  //hash값.mp4 로 파일 저장, splited[splited.length-1]이 "mp4"임
+////        System.out.println("saved Name: "+saveName);
+//        String path=System.getProperty("user.home")+"\\GaoriVideos"; //여기서 user.home: 사용자의 홈 디렉토리(~)
+//        // -> 저같은 경우에는 User/users 라서  Users/user/GaoriVideos 폴더안에 비디오가 저장됨니다
+//
+//        if(!new File(path).exists()){  //위의 폴더가 없다면 새로 생성해줌
+//            try{
+//                new File(path).mkdir();
+//            }
+//            catch (Exception e){
+//                e.getStackTrace();
+//            }
+//        }
+//        String filePath = path + "\\" + saveName; //해당 경로에 hash값+mp4 의 파일을 새로 생성해서
+//        files.transferTo(new File(filePath));//실제로 저장시켜줌
+//
+//        Long id;
+//        try{
+//            id =videoService.saveUploadedVideo(origName,saveName,filePath); //업로드 하고 성공 시, DB에 저장된 id값 반환
+//        }catch (Exception e){
+//            System.out.println(e);
+//            return "redirect:/upload"; //DB에 저장 실패시, upload로 redirect
+//        }
+////        System.out.println("saved file id: "+id);
+//        return "redirect:/upload/"+id.toString(); //업로드 한 영상 제목 보여주는 페이지 (바로 아래)로 redirect
+//    }
+
     //파일 업로드 후, submit 클릭 시 여기로 이동
     @PostMapping("/uploadVideo")
-    public String saveVideo(@RequestParam("file") MultipartFile files) throws IOException {
+    public ModelAndView saveVideo(@RequestParam("file") MultipartFile files) throws IOException {
         String origName=files.getOriginalFilename();
-//        System.out.println("original name: "+origName);
         String[] splited=origName.split("\\.");
         String saveName=new GenerateHash(origName).out()+"."+splited[splited.length-1];  //hash값.mp4 로 파일 저장, splited[splited.length-1]이 "mp4"임
-//        System.out.println("saved Name: "+saveName);
         String path=System.getProperty("user.home")+"\\GaoriVideos"; //여기서 user.home: 사용자의 홈 디렉토리(~)
         // -> 저같은 경우에는 User/users 라서  Users/user/GaoriVideos 폴더안에 비디오가 저장됨니다
 
@@ -60,28 +91,10 @@ public class VideoController {
             id =videoService.saveUploadedVideo(origName,saveName,filePath); //업로드 하고 성공 시, DB에 저장된 id값 반환
         }catch (Exception e){
             System.out.println(e);
-            return "redirect:/upload"; //DB에 저장 실패시, upload로 redirect
         }
-//        System.out.println("saved file id: "+id);
-        return "redirect:/upload/"+id.toString(); //업로드 한 영상 제목 보여주는 페이지 (바로 아래)로 redirect
-    }
 
-    @GetMapping("/upload/{id}")
-    public String uploadResult(@PathVariable("id") Long id, Model model){
-        UploadedVideoFile video=videoService.findUploadedVideoById(id); //id값으로 video객체 찾아와서
-        if(video!=null) {
-            model.addAttribute("video",video); //model에 저장 -> 나중에 html파일에서 이름 표시!
-            return "uploadSuccess";
-        }
-        else//DB에서 해당 id값으로 저장된 data를 찾을 수 없다면, upload로 redirect
-            return "redirect:/upload";
-    }
-
-    @GetMapping(value = "/test.do")
-    public ModelAndView Test() {
         ModelAndView mav = new ModelAndView();
-
-        String url = "http://127.0.0.1:5000/tospring";
+        String url = "http://127.0.0.1:5000/getVideo";
         String sb = "";
         try {
             HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
@@ -106,10 +119,57 @@ public class VideoController {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        mav.addObject("tests", sb.toString()); // "test1"는 jsp파일에서 받을때 이름,
+
+        mav.addObject("video", sb.toString());
         //sb.toString은 value값(여기에선 test)
         mav.addObject("fail", false);
         mav.setViewName("test");   // jsp파일 이름
+        System.out.println("hi");
+        return mav;
+    }
+
+    @GetMapping("/upload/{id}")
+    public String uploadResult(@PathVariable("id") Long id, Model model){
+        UploadedVideoFile video=videoService.findUploadedVideoById(id); //id값으로 video객체 찾아와서
+        if(video!=null) {
+            model.addAttribute("video",video); //model에 저장 -> 나중에 html파일에서 이름 표시!
+            return "uploadSuccess";
+        }
+        else//DB에서 해당 id값으로 저장된 data를 찾을 수 없다면, upload로 redirect
+            return "redirect:/upload";
+    }
+
+    @GetMapping(value = "/test.do") // flask -> spring 테스트 코드
+    public ModelAndView Test() {
+        ModelAndView mav = new ModelAndView();
+
+        String url = "http://127.0.0.1:5000/tospring"; // flask에서 데이터를 보낸 url
+        String sb = "";
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                sb = sb + line + "\n";
+            }
+            System.out.println("========br======" + sb.toString());
+            if (sb.toString().contains("ok")) {
+                System.out.println("test");
+            }
+            br.close();
+            System.out.println("" + sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mav.addObject("tests", sb.toString()); //
+        //sb.toString은 value값(여기에선 test)
+        mav.addObject("fail", false);
+        mav.setViewName("test");   // templates 이름
         System.out.println("hi");
         return mav;
     }
