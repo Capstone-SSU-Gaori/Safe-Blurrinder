6e58a9f40614ffc1cdadf94596620bca2377db27
Song-EunJu <jd06280@naver.com>
2021-11-29 17:28:32 +0900

    [feat] flask/spring 연동

--- src/main/java/com/example/SafeBlurrinder/controller/VideoController.java
+++ src/main/java/com/example/SafeBlurrinder/controller/VideoController.java
@@ -7,14 +7,17 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Controller
 public class VideoController {
@@ -72,5 +75,42 @@ public class VideoController {
         }
         else//DB에서 해당 id값으로 저장된 data를 찾을 수 없다면, upload로 redirect
             return "redirect:/upload";
+    }
+
+    @GetMapping(value = "/test.do")
+    public ModelAndView Test() {
+        ModelAndView mav = new ModelAndView();
+
+        String url = "http://127.0.0.1:5000/tospring";
+        String sb = "";
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                sb = sb + line + "\n";
+            }
+            System.out.println("========br======" + sb.toString());
+            if (sb.toString().contains("ok")) {
+                System.out.println("test");
+
+            }
+            br.close();
+            System.out.println("" + sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        mav.addObject("tests", sb.toString()); // "test1"는 jsp파일에서 받을때 이름,
+        //sb.toString은 value값(여기에선 test)
+        mav.addObject("fail", false);
+        mav.setViewName("test");   // jsp파일 이름
+        System.out.println("hi");
+        return mav;
     }
 }
