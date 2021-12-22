0fa2ed2528be257cfc15a0b11b704f63e4102023
안채완 <wani9909@naver.com>
2021-12-23 02:43:16 +0900

    Create selectFaceController.java

---
+++ src/main/java/com/example/SafeBlurrinder/controller/selectFaceController.java
@@ -0,0 +1,13 @@
+package com.example.SafeBlurrinder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class selectFaceController {
+
+    @GetMapping("/select") //localhost:8080/select하면 화면나옴
+    public String selectFace() {
+        return "selectFace";
+    }
+}
