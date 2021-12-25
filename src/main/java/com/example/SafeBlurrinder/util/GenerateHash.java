d4b8fe78146f1f69872c5f8f124e0061676ef280
somsom13 <somin1113@naver.com>
2021-12-26 00:44:19 +0900

    [FIX] 영상 이름 저장 시 hash값 랜덤성 부여

--- src/main/java/com/example/SafeBlurrinder/util/GenerateHash.java
+++ src/main/java/com/example/SafeBlurrinder/util/GenerateHash.java
@@ -2,6 +2,7 @@ package com.example.SafeBlurrinder.util;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 
 public class GenerateHash {
     //비디오 저장할 때, 원본 이름과 다르게 MD5로 hash값 생성해서 저장
@@ -11,7 +12,13 @@ public class GenerateHash {
 
         try {
             MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(input.getBytes("utf8"));
+            SecureRandom rand=new SecureRandom();
+            byte[] saltBytes=new byte[16];
+            rand.nextBytes(saltBytes);
+            String salt=new String(saltBytes);
+            String msg=input+salt;
+
+            digest.update(msg.getBytes("utf8"));
             byte[] hashByte=digest.digest();
             StringBuilder builder = new StringBuilder();
             for(byte b : hashByte) {
